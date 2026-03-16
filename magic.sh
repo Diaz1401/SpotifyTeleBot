1083c9b13e619167e5d288a0ad03ee7f3cd4315e
Diaz Nuraji <86863028+Diaz1401@users.noreply.github.com>
2026-03-16 09:54:57 +0700

    magic.sh: Removed bitrate specification for m4a and mp3 formats

--- magic.sh
+++ magic.sh
@@ -8,9 +8,9 @@ fi
 if [[ $2 == "flac" ]]; then #flac
         spotdl "$1" --format flac --ffmpeg-args '-ar 44100'
 elif [[ $2 == "m4a" ]]; then #m4a
-        spotdl "$1" --format m4a --bitrate 320k --ffmpeg-args '-ar 44100'
+        spotdl "$1" --format m4a --ffmpeg-args '-ar 44100'
 elif [[ $2 == "mp3" ]]; then #mp3
-        spotdl "$1" --format mp3 --bitrate 320k --ffmpeg-args '-ar 44100'
+        spotdl "$1" --format mp3 --ffmpeg-args '-ar 44100'
 elif [[ $2 == "-sc" ]]; then # soundcloud
         scdl -l "$1"
         MUSIC=$(ls | grep .mp3)
