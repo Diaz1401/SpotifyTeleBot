4abdf51aad84f1413bfc0c27fd5ce329b61d19f1
Diaz Nuraji <86863028+Diaz1401@users.noreply.github.com>
2026-03-16 10:10:25 +0700

    use venv

--- magic.sh
+++ magic.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
+SPOTDL=./.venv/bin/spotdl
+
 if [[ $3 == "" ]]; then
         echo "Please send a link / Query!" > link.txt
         exit
 fi
 
 if [[ $2 == "flac" ]]; then #flac
-        spotdl "$1" --format flac --ffmpeg-args '-ar 44100'
+        $SPOTDL "$1" --format flac --ffmpeg-args '-ar 44100'
 elif [[ $2 == "m4a" ]]; then #m4a
-        spotdl "$1" --format m4a --ffmpeg-args '-ar 44100'
+        $SPOTDL "$1" --format m4a --ffmpeg-args '-ar 44100'
 elif [[ $2 == "mp3" ]]; then #mp3
-        spotdl "$1" --format mp3 --ffmpeg-args '-ar 44100'
+        $SPOTDL "$1" --format mp3 --ffmpeg-args '-ar 44100'
 elif [[ $2 == "-sc" ]]; then # soundcloud
-        scdl -l "$1"
+        $SPOTDL -l "$1"
         MUSIC=$(ls | grep .mp3)
 fi
 
